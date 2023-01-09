26ef216823a0635042fbe43ff4cb80dd75379fe8
Steven <107305979+Stiefanek@users.noreply.github.com>
2023-01-09 14:03:41 +0100

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,6 +2,8 @@ namespace MonkoReanimated;
 
 public static class ModHelperData
 {
+    public const string WorksOnVersion = "34.3";
+
     public const string Version = "1.0.0";
     public const string Name = "MonkoReanimated";
 
